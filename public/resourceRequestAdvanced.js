5b10014459bef0a34c54bcc14b0d2600e53f0052
Richard Smith <richard.h.smith@cyberdefense.global>
2023-09-18 11:11:59 +0100

    Added support for ANY Lookups, working on adding other requests and fixing UI.

--- public/resourceRequestAdvanced.js
+++ public/resourceRequestAdvanced.js
@@ -12,23 +12,29 @@ fetch('/fetchLookupResults')
       element = document.getElementById("advLookupResultsList");
       console.log(object.lookups.length);
       console.log(object.lookups);
-      for (var result = 0; result < object.lookups.length; result++){
-        para = document.createElement("p");
-        resourceType = object.lookups[result].type;
-        if (resourceType == 'A'){
-              node = document.createTextNode(`IPv4: ${object.lookups[result].address}`);
-        } else if (resourceType == 'AAAA') {
-              node = document.createTextNode(`IPv6: ${object.lookups[result].address}`);      
-        } else if (resourceType == 'MX') {
-              node = document.createTextNode(`MX: ${object.lookups[result].exchange}`);
-        } else if (resourceType == 'NS') {
-              node = document.createTextNode(`NS: ${object.lookups[result].value}`)
-        } else if (resourceType == 'TXT'){
-              node = document.createTextNode(`TXT: ${object.lookups[result].entries}`);
-        } else if (resourceType == 'SOA'){
-              node = document.createTextNode(`SOA: ${object.lookups[result].hostmaster}`);
+
+        if (object.lookups[object.lookups.length-1] == 'ANY'){
+            for (var result = 0; result < object.lookups.length; result++){
+                para = document.createElement("p");
+                resourceType = object.lookups[result].type;
+                if (resourceType == 'A'){
+                    node = document.createTextNode(`IPv4: ${object.lookups[result].address}`);
+                } else if (resourceType == 'AAAA') {
+                    node = document.createTextNode(`IPv6: ${object.lookups[result].address}`);      
+                } else if (resourceType == 'MX') {
+                    node = document.createTextNode(`MX: ${object.lookups[result].exchange}`);
+                } else if (resourceType == 'NS') {
+                    node = document.createTextNode(`NS: ${object.lookups[result].value}`)
+                } else if (resourceType == 'TXT'){
+                    node = document.createTextNode(`TXT: ${object.lookups[result].entries}`);
+                } else if (resourceType == 'SOA'){
+                    node = document.createTextNode(`SOA: ${object.lookups[result].hostmaster}`);
+                }
+                para.appendChild(node);
+                element.appendChild(para);
+            }
+        } else if (object.lookups[object.lookups.length-1] == 'NS'){
+                para = document.createElement("p");
+                resourceType = object.lookups[result].type;
         }
-        para.appendChild(node);
-        element.appendChild(para);
-      }
   });
